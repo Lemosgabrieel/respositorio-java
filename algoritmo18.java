e19617e934c408e28497c544af87b06c00ff1f52
Gabriel Lemos da costa Gonçalves de Oliveira <biellemosgon@hotmail.com>
2025-07-27 22:17:43 -0300

    algoritmo de multiplo de 10
    algoritmo que imprime numeros de 10 em 10 ate 100, e a cada dez numeros, diz qual é o número múltiplo de 10.

---
+++ algoritmo18.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class algoritmo18 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            if (i % 10 == 0) {
+                System.out.println("multiplo de 10");
+            } 
+        }     
+    }
+}        
+
